7f850b9575de42d459c3fa581a1fa9176bf7d0df
YoqubovDev <xiyobidasturchi0011@gmail.com>
2025-03-22 09:54:00 +0500

    sentry add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Sentry\State\HubInterface;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/sentry-test', function (HubInterface $sentry) {
+    $sentry->captureException(new Exception('Bu Laravel uchun test xatosi!'));
+    return 'Xato Sentry-ga yuborildi!';
 });
